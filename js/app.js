fa2db89561b95aff08e3e9a5e761244a9f167dbe
Ivan Balmaceda <balmacedaivan5@gmail.com>
2021-11-08 23:18:01 -0300

    Efecto de Background-color con scroll

--- js/app.js
+++ js/app.js
@@ -1,1 +1,14 @@
-console.log('holaaaaa')
+/*background-color navbar scrolling*/
+function changeBg(){
+    let navbar = document.getElementById('navbar');
+    let scrollValue = window.scrollY;
+
+    if( scrollValue < 150 ){
+        navbar.classList.remove('bgColor');
+    } else{
+        navbar.classList.add('bgColor')
+    }
+
+}
+
+window.addEventListener('scroll', changeBg);
